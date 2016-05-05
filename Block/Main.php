f798cc9bf9a8bae8b1d5755b3c6e2093173e5bd2
shubham03mathur <shubham@excellencetechnologies.in>
2016-05-05 19:22:43 +0530

    Grid

--- Block/Main.php
+++ Block/Main.php
@@ -5,18 +5,36 @@ class Main extends \Magento\Framework\View\Element\Template
     protected $_testFactory;
     public function __construct(
         \Magento\Framework\View\Element\Template\Context $context,
-        \Excellence\Table\Model\TestFactory $testFactory
+        \Excellence\Table\Model\TestFactory $testFactory,
+        array $data = []
     )
     {
+        
         $this->_testFactory = $testFactory;
-        parent::__construct($context);
-    }
-    protected function _prepareLayout()
-    {
-       
+        parent::__construct($context, $data);
         $test = $this->_testFactory->create()->getCollection()->setOrder('excellence_table_test_id','desc');
         $this->setTestModel($test);
+    }
 
+    protected function _prepareLayout()
+    {
+        parent::_prepareLayout();
+        if($this->getTestModel())
+        {    
+            $pager = $this->getLayout()->createBlock(
+                 'Magento\Theme\Block\Html\Pager',
+                 'sales.order.history.pager'
+            );
+            $pager->setLimit(5)->setShowAmounts(false)->setTestModel($this->getTestModel());
+            $this->setChild('pager', $pager);
+            $this->getTestModel()->load();    
+        } 
+        return $this;  
+
+    }
+   public function getPagerHtml()
+    {
+        return $this->getChildHtml('pager');
     }
 	public function getEditUrl($id)
     {
